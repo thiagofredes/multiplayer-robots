79189469989d26112693dca083ea6c69944fe7c0
Thiago <trodrigues02@gmail.com>
2018-03-16 23:57:49 -0300

    Include player idle and run and gun animations. Include weapon models. Add mouse aiming script. Create preliminar IK aiming mechanics.

---
+++ Assets/Scripts/AimingIK.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimingIK : MonoBehaviour
+{
+
+    public Transform playerHeading;
+
+    public float lookAtDistance;
+
+    public string[] layerToIgnoreOnRaycast;
+
+    private Animator _animator;
+
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void OnAnimatorIK(int layer)
+    {
+        RaycastHit aimHit;
+        Vector3 viewportPoint = new Vector3(0.5f, 0.5f, 0.5f);
+        Ray screenRay = Camera.main.ViewportPointToRay(viewportPoint);
+
+        if (Physics.Raycast(screenRay.origin, ThirdPersonCameraController.CameraForward, out aimHit, Camera.main.farClipPlane, ~LayerMask.GetMask(layerToIgnoreOnRaycast)))
+        {
+            _animator.SetLookAtWeight(1f, 0.8f, 1f, 1f, 0.5f);
+            _animator.SetLookAtPosition(aimHit.point);
+        }
+        else
+        {
+            _animator.SetLookAtWeight(1f, 0.8f, 1f, 1f, 0.5f);
+            _animator.SetLookAtPosition(playerHeading.position + ThirdPersonCameraController.CameraForward * lookAtDistance);
+        }
+    }
+}
---
+++ Assets/Scripts/LockMouse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockMouse : MonoBehaviour {
+
+	public bool cursorVisible;
+
+	// Use this for initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = cursorVisible;
+	}
+}
